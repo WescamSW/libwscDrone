d163d4f028edc9c744328b929d7f6882e308aaaa
WescamSW <49984794+WescamSW@users.noreply.github.com>
2019-05-01 19:38:58 -0400

    Sjl/fix camera photos (#5)
    * Fixed issue with video freezing due to use of photos
    
    * more photo fixes

--- src/CameraControl.cpp
+++ src/CameraControl.cpp
@@ -4,6 +4,7 @@
  *  Created on: Feb 3, 2019
  *      Author: slascos
  */
+#include <iostream>
 #include <stdexcept>
 #include "Utils.h"
 #include "CameraControl.h"
@@ -12,14 +13,15 @@ using namespace std;
 
 namespace wscDrone {
 
-CameraControl::CameraControl(std::shared_ptr<DroneController> droneController, PhotoType photoType)
+constexpr int PHOTO_TIMEOUT_MS = 10000;
+
+CameraControl::CameraControl(std::shared_ptr<DroneController> droneController, std::shared_ptr<VideoDriver> videoDriver)
 {
     m_deviceController = droneController->getDeviceController();
+    m_videoDriver = videoDriver;
     if (!m_deviceController) {
         throw runtime_error("CameraControl(...): invalid deviceController");
     }
-
-    m_photoType = photoType;
 }
 
 void CameraControl::setTiltPan(float tilt, float pan)
@@ -36,6 +38,33 @@ void CameraControl::setForward(void)
         throw runtime_error("CameraControl::setForward(): invalid device controller");
     }
     m_deviceController->aRDrone3->sendCameraOrientationV2(m_deviceController->aRDrone3, 0.0f, 0.0f);
+    waitMilliseconds(300);
+}
+
+void CameraControl::setPhotoType(PhotoType photoType)
+{
+    switch(photoType)
+    {
+    case PhotoType::RAW :
+        m_deviceController->aRDrone3->sendPictureSettingsPictureFormatSelection(
+                m_deviceController->aRDrone3, ARCOMMANDS_ARDRONE3_PICTURESETTINGS_PICTUREFORMATSELECTION_TYPE_RAW);
+        break;
+
+    case PhotoType::JPEG_4_3 :
+        m_deviceController->aRDrone3->sendPictureSettingsPictureFormatSelection(
+                m_deviceController->aRDrone3, ARCOMMANDS_ARDRONE3_PICTURESETTINGS_PICTUREFORMATSELECTION_TYPE_JPEG);
+        break;
+
+    case PhotoType::SNAPSHOT :
+        m_deviceController->aRDrone3->sendPictureSettingsPictureFormatSelection(
+                m_deviceController->aRDrone3, ARCOMMANDS_ARDRONE3_PICTURESETTINGS_PICTUREFORMATSELECTION_TYPE_SNAPSHOT);
+        break;
+
+    case PhotoType::FISHEYE :
+        m_deviceController->aRDrone3->sendPictureSettingsPictureFormatSelection(
+                m_deviceController->aRDrone3, ARCOMMANDS_ARDRONE3_PICTURESETTINGS_PICTUREFORMATSELECTION_TYPE_JPEG_FISHEYE);
+        break;
+    }
 }
 
 void CameraControl::capturePhoto()
@@ -44,15 +73,80 @@ void CameraControl::capturePhoto()
         throw runtime_error("CameraControl::setForward(): invalid device controller");
     }
 
-    if (m_photoType == PhotoType::RAW) {
-        m_deviceController->aRDrone3->sendPictureSettingsPictureFormatSelection(
-                m_deviceController->aRDrone3, ARCOMMANDS_ARDRONE3_PICTURESETTINGS_PICTUREFORMATSELECTION_TYPE_RAW);
-    } else {
-        m_deviceController->aRDrone3->sendPictureSettingsPictureFormatSelection(
-                m_deviceController->aRDrone3, ARCOMMANDS_ARDRONE3_PICTURESETTINGS_PICTUREFORMATSELECTION_TYPE_JPEG);
-    }
-    waitMilliseconds(100);
+    printf("CameraControl::capturePhoto is stopping video stream to take a photo\n");
+    ARCONTROLLER_Device_StopVideoStream(m_deviceController);
+
+    //while ( m_videoDriver->getVideoState() != VideoState::STOPPED) {} // Wait until video is stopped
+    waitMilliseconds(500); // wait until video is stopped
+
     m_deviceController->aRDrone3->sendMediaRecordPictureV2(m_deviceController->aRDrone3);
+    m_waitPhotoComplete(); // Block until photo complete
+}
+
+void CameraControl::m_notifyPhotoComplete()
+{
+    //m_photoSem.notify();
+}
+
+bool CameraControl::m_waitPhotoComplete() {
+
+    //cout << "***Waiting for the photo to start" << endl;
+//    while (m_photoInProgress == false) {
+//
+//    } // wait until the photo starts
+//
+//    cout << "Waiting on photoSem..." << endl;
+//    bool timeout = !m_photoSem.waitTimed(PHOTO_TIMEOUT_MS);
+//    if (timeout) { cout << "TIMEOUT while waiting on camera state semaphore" << endl; }
+//
+////    while (m_photoInProgress == true) {
+////        cout << "***Waiting for the photo to finish" << endl;
+////    } // wait until the photo starts
+
+    cout << "Wait 2 seconds for the photo to start" << endl;
+    waitSeconds(2);
+    int timeoutCount = 10;
+    while ((m_cameraState == CameraState::BUSY) && (timeoutCount > 0)) {
+        timeoutCount--;
+        cout << "...waiting another second for the photo to finished" << endl;
+        waitSeconds(1);
+    }
+    bool timeout = false;
+    if (m_cameraState == CameraState::BUSY) {
+        cout << "ERROR: the camera seems stuck in BUSY!!!" << endl;
+        timeout = true;
+    }
+
+//    cout << "Wait 5 seconds for photo to finish before restarting the stream" << endl;
+//    waitSeconds(5);
+//    if (m_cameraState == CameraState::BUSY) {
+//        cout << "...camera is still busy, waiting 5 more seconds..." << endl;
+//        waitSeconds(5);
+//    }
+//    bool timeout = false;
+//    if (m_cameraState == CameraState::BUSY) {
+//        cout << "ERROR: the camera seems stuck in BUSY!!!" << endl;
+//        timeout = true;
+//    }
+
+    printf("CameraControl::m_waitPhotoComplete() is re-starting video stream after completing a photo\n");
+    ARCONTROLLER_Device_StartVideoStream(m_deviceController);
+
+    return timeout;
+}
+
+void CameraControl::setCameraState(CameraState cameraState)
+{
+    if ((m_photoInProgress==false) && (cameraState==CameraState::BUSY)) {
+        //cout << "Photo in progress" << endl;
+        m_photoInProgress = true;
+    }
+    else if ((m_photoInProgress==true) && (cameraState==CameraState::READY)) {
+        m_photoInProgress = false;
+        //cout << "photo is complete" << endl;
+        m_notifyPhotoComplete();
+    }
+    m_cameraState = cameraState;
 }
 
 } // wscDrone
